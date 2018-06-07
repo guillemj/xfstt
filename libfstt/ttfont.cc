guillemj/xfstt

commit 51d1af7d21159f7c7390d883f7c905ea32f46219

Switch few more 'char *' to C++ string

libfstt/ttfont.cc
@@ -2,6 +2,7 @@
  * General handling of *ttf files
  *
  * Copyright (C) 1997-1998 Herbert Duerr
+ * Copyright (C) 2008 Guillem Jover
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -233,25 +234,17 @@ TTFont::getFontInfo(FontInfo *fi)
 			fi->panose[i] = 0;		// any
 	}
 
-	const char *faceName;
-	char *p_faceName = nameTable->getString(1, 4, &fi->faceLength);
+	string faceName = nameTable->getString(1, 4);
 
-	if (p_faceName) {
-		faceName = p_faceName;
-	} else {
+	if (faceName.empty())
 		faceName = "Unknown";
-		fi->faceLength = strlen(faceName);
-	}
 
-	if (fi->faceLength > 32)
-		fi->faceLength = 32;
+	if (fi->faceLength > sizeof(fi->faceName))
+		fi->faceLength = sizeof(fi->faceName);
+	else
+		fi->faceLength = faceName.size();
 
-	strncpy(fi->faceName, faceName, fi->faceLength);
-	if (p_faceName)
-		delete faceName;
-
-	if (fi->faceLength < 31)
-		fi->faceName[fi->faceLength] = 0;
+	faceName.copy(fi->faceName, fi->faceLength);
 }
 
 
@@ -410,27 +403,14 @@ TTFont::getXLFDbase(string xlfd_templ)
 //#define XLFDEXT "-normal-tt-0-0-0-0-p-0-iso8859-1"
 //#define XLFDEXT "-normal-tt-"
 
-	int lenFamily;
-	char *p_strFamily = nameTable->getString(1, 1, &lenFamily);
-	string strFamily;
+	string strFamily = nameTable->getString(1, 1);
+	string strSubFamily = nameTable->getString(1, 2);
 
-	if (p_strFamily) {
-		strFamily = string(p_strFamily, lenFamily);
-	} else {
+	if (strFamily.empty())
 		strFamily = "unknown";
-		lenFamily = strFamily.length();
-	}
 
-	int lenSub;
-	char *p_strSubFamily = nameTable->getString(1, 2, &lenSub);
-	string strSubFamily;
-
-	if (p_strFamily) {
-		strSubFamily = string(p_strSubFamily, lenSub);
-	} else {
+	if (strFamily.empty())
 		strSubFamily = "tt";
-		lenSub = strSubFamily.length();
-	}
 
 	string::iterator i;
 
@@ -455,11 +435,6 @@ TTFont::getXLFDbase(string xlfd_templ)
 
 	for (i = xlfd.begin(); i < xlfd.end(); i++)
 		*i = std::tolower(*i);
-
-	if (p_strFamily)
-		delete p_strFamily;
-	if (p_strSubFamily)
-		delete p_strSubFamily;
 
 	debug("xlfd = \"%s\"\n", xlfd.c_str());
 

libfstt/table_name.cc
@@ -2,6 +2,7 @@
  * Name Table
  *
  * Copyright (C) 1997-1998 Herbert Duerr
+ * Copyright (C) 2008 Guillem Jover
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -29,8 +30,8 @@ NameTable::NameTable(RandomAccessFile &f, int offset, int length):
 	strBase = readUShort();		// start of string offset
 }
 
-char *
-NameTable::getString(int pfId, int strId, int *pLen)
+string
+NameTable::getString(int pfId, int strId)
 {
 	// name records
 	seekAbsolute(6);
@@ -63,39 +64,30 @@ NameTable::getString(int pfId, int strId, int *pLen)
 
 		if (platformId == pfId && nameId == strId) {
 			char *p = (char *)base + strBase + strOffset;
-#if 0
-			for (int j = 0; j < strLength; ++j)
-				debug("%c", *(p++));
-			debug("\n");
-#endif
-			*pLen = strLength;
-			p = (char *)base + strBase + strOffset;
 
 			if (p <= (char *)base)
 				return 0;
 			if (p >= (char *)base + getLength())
 				return 0;
 
-			char *str = new char [*pLen];
-			memcpy(str, p, *pLen);
-
-			return str;
+			return string(p, strLength);
 		}
 	}
 
 	// hack to convert unicode -> ascii
 	if (pfId == 1) {
-		const char *p = getString(3, strId, pLen);
+		const string p = getString(3, strId);
 
-		if (!p)
+		if (p.empty())
 			return 0;
 
-		*pLen >>= 1;
-		char *convbuf = new char [*pLen];
-		for (int i = *pLen; --i >= 0;)
-			convbuf[i] = p[2 * i + 1];
+		int len = p.size() >> 1;
 
-		delete p;
+		string convbuf;
+		convbuf.reserve(len);
+
+		for (int i = len; --i >= 0;)
+			convbuf[i] = p[2 * i + 1];
 
 		return convbuf;
 	}

libfstt/ttf.h
@@ -311,7 +311,7 @@ public:
 		NAME_TRADEMARK = 7
 	};
 
-	char *getString(int platformId, int strId, int *len);
+	string getString(int platformId, int strId);
 };
 
 // Font specific flags

src/encoding.cc
@@ -2,6 +2,7 @@
  * Remap from unicode to other encodings
  *
  * Copyright (C) 1998 Herbert Duerr
+ * Copyright (C) 2008 Guillem Jover
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -20,7 +21,6 @@
  */
 
 #include "encoding.h"
-#include <string.h>
 
 // Map box drawing gliphs to 0x00 .. 0x1f for curses based program
 static unsigned short boxtable[32] = {
@@ -33,8 +33,8 @@ static unsigned short boxtable[32] = {
 Encoding *Encoding::first = 0;
 Encoding *Encoding::last = 0;
 
-Encoding::Encoding(const char *mapname):
-	strName(mapname), lenName(strlen(mapname))
+Encoding::Encoding(const string mapname):
+	Name(mapname)
 {
 	if (!first)
 		first = this;
@@ -45,34 +45,49 @@ Encoding::Encoding(const char *mapname):
 	next = 0;
 }
 
+static string
+string_token(string str, string sep, unsigned int &pos)
+{
+	string ret;
+	unsigned int n = str.find_first_of(sep, pos);
+
+	if (n == string::npos) {
+		pos = n;
+	} else {
+		ret = string(str, pos, n - pos);
+		pos = n + 1;
+	}
+
+	return ret;
+}
+
 int
-Encoding::parse(char *mapnames, Encoding **maps0, int maxcodes)
+Encoding::parse(string mapnames, Encoding **maps0, int maxcodes)
 {
 	Encoding **maps = maps0;
 	Encoding *m;
-	char *mapname;
+	string mapname;
+	unsigned int pos = 0;
 
-	mapname = strtok(mapnames, ", ");
-	while (mapname && maps - maps0 < maxcodes) {
+	do {
+		mapname = string_token(mapnames, ", ", pos);
+
 		m = find(mapname);
-		if (m) {
+		if (m)
 			*(maps++) = m;
-			mapname = strtok(NULL, ", ");
-		} else {
-			mapname = strtok(NULL, ", ");
-		}
-	}
+	} while (pos != string::npos && maps - maps0 < maxcodes);
+
 	return (maps - maps0);
 }
 
 // Search the list of encodings for an encoding with the given name.
 Encoding *
-Encoding::find(char *mapname)
+Encoding::find(string mapname)
 {
 	Encoding *m;
 
 	for (m = first; m; m = m->next) {
-		if (!strcmp(mapname, m->strName)) {
+		if (mapname == m->Name) {
 			return m;
 		}
 		if (m == last) {

src/encoding.h
@@ -22,21 +22,24 @@
 #ifndef ENCODING_H
 #define ENCODING_H
 
+#include <string>
+
+using std::string;
+
 class Encoding {
 public:
 	virtual ~Encoding() {}
 	virtual int map2unicode(int code) = 0;
 	virtual int hasGlyphs(int /*unicodeRange*/[4]) { return 1; }
 	static void getDefault(Encoding **maps, int maxcodes);
-	static int parse(char *mapnames, Encoding **maps, int maxcodes);
-	static Encoding *find(char *mapname);
+	static int parse(string mapnames, Encoding **maps, int maxcodes);
+	static Encoding *find(string mapname);
 	static Encoding *enumerate(Encoding *iterator);
 
-	const char *strName;
-	const int lenName;
+	const string Name;
 
 protected:
-	Encoding(const char *name);
+	Encoding(const string name);
 
 private:
 	static Encoding *first, *last;

src/perftest.cc
@@ -2,6 +2,7 @@
  * Test ttf engine performance
  *
  * Copyright (C) 1997-1998 Herbert Duerr
+ * Copyright (C) 2008 Guillem Jover
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -49,13 +50,15 @@ ttPerfDir(Rasterizer *raster, int pt, FontExtent *fe, char *ttdir)
 	DIR *dirp = opendir(".");
 
 	while (dirent *de = readdir(dirp)) {
-		int namelen = strlen(de->d_name);
+		string filename(de->d_name);
+		int namelen = filename.size();
+
 		if (namelen - 4 <= 0) continue;
-		char *ext = &de->d_name[namelen - 4];
-		if (ext[0] != '.') continue;
-		if (tolower(ext[1]) != 't') continue;
-		if (tolower(ext[2]) != 't') continue;
-		if (tolower(ext[3]) != 'f') continue;
+
+		string ext = string(filename, namelen - 4, namelen);
+
+		if (ext != ".ttf")
+			continue;
 
 		struct stat statbuf;
 		stat(de->d_name, &statbuf);

src/xfstt.cc
@@ -163,7 +163,7 @@ usage(int verbose)
 "  --inetd              run as inetd service\n"
 "  --help               show this help message\n"
 "  --version            show the version\n"
-"\n"), default_port, FONTDIR, CACHEDIR, PIDFILE, encodings[0]->strName);
+"\n"), default_port, FONTDIR, CACHEDIR, PIDFILE, encodings[0]->Name.c_str());
 
 	printf(_(
 "Attach to X Server by \"xset fp+ unix/:%i\"\n"
@@ -382,7 +382,7 @@ listXLFDFonts(char *pattern0, int index, char *buf)
 		xlfdExt[8] = proportion;
 		strcpy(buf, xlfdName);
 		strcpy(buf + ttfn->xlfdLen, xlfdExt);
-		strcpy(buf + ttfn->xlfdLen + 12, encodings[mapIndex]->strName);
+		strcpy(buf + ttfn->xlfdLen + 12, encodings[mapIndex]->Name.c_str());
 		if (!encodings[++mapIndex]) mapIndex = 0;
 		*buf0 = strlen(buf);
 		return *buf0 + 1;
@@ -414,8 +414,8 @@ listXLFDFonts(char *pattern0, int index, char *buf)
 	*buf = 0;
 	// XXX: this hack satisfies Mozilla, thanks Andrew Turner
 	if (!strcmp(buf - 4, "-0-0")) {
-		strcpy(buf - 3, encodings[mapIndex]->strName);
-		buf += encodings[mapIndex]->lenName - 3;
+		strcpy(buf - 3, encodings[mapIndex]->Name.c_str());
+		buf += encodings[mapIndex]->Name.size() - 3;
 		if (!encodings[++mapIndex])
 			mapIndex = 0;
 	} else
@@ -741,7 +741,7 @@ openXLFD(Rasterizer *raster, char *xlfdName, FontParams *fp, int fid)
 	if (!encoding)
 		encoding = encodings[0];
 
-	debug("\nopenXLFD(\"%s\"), %s\n", xlfdName, encoding->strName);
+	debug("\nopenXLFD(\"%s\"), %s\n", xlfdName, encoding->Name.c_str());
 	debug("size %d, resx %d, resy %d\n",
 	      fp->point[0], fp->resolution[0], fp->resolution[1]);
 
@@ -1972,7 +1972,7 @@ main(int argc, char **argv)
 					maps = Encoding::enumerate(maps);
 					if (!maps)
 						exit(0);
-					info("\t%s\n", maps->strName);
+					info("\t%s\n", maps->Name.c_str());
 				}
 			}
 		} else if (!strcmp(argv[i], "--help")) {

commit 2d6e7e9a45c34f8d350a27cfdc05c0ff6ee81c45

libfstt: Do not use uninitialized fi.faceLength
We should be using the faceName.size() instead.

Fixes: commit 51d1af7d21159f7c7390d883f7c905ea32f46219
Warned-by: coverity

libfstt/ttfont.cc
@@ -238,7 +238,7 @@ TTFont::getFontInfo(FontInfo *fi)
 	if (faceName.empty())
 		faceName = "Unknown";
 
-	if (fi->faceLength > sizeof(fi->faceName))
+	if (faceName.size() > sizeof(fi->faceName))
 		fi->faceLength = sizeof(fi->faceName);
 	else
 		fi->faceLength = faceName.size();